96694dc5027031a78054832e09d01ab1d0b09629
bhenrymicrosoft <bhenry@microsoft.com>
2021-11-09 13:03:54 +0000

    Updated privacy page to animals page.

--- MvcContozoo/Controllers/HomeController.cs
+++ MvcContozoo/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace MvcContozoo.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Animals()
         {
             return View();
         }
