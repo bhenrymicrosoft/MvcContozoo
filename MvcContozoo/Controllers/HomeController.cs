386cf4902adcba145d46bae79d5b5b89d4ffede8
bhenrymicrosoft <bhenry@microsoft.com>
2021-11-16 14:14:49 +0000

    Connecting API to APP

--- MvcContozoo/Controllers/HomeController.cs
+++ MvcContozoo/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace MvcContozoo.Controllers
 {
@@ -18,14 +21,37 @@ namespace MvcContozoo.Controllers
             _logger = logger;
         }
 
+        //string Baseurl = "https://localhost:44361/";
+        string Baseurl = "https://contozoozooapi.azurewebsites.net/";
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Animals()
+        public async Task<ActionResult> Animals()
         {
-            return View();
+            List<Animals> EmpInfo = new List<Animals>();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient
+                HttpResponseMessage Res = await client.GetAsync("api/Animals");
+                //Checking the response is successful or not which is sent using HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+                    //Deserializing the response recieved from web api and storing into the Employee list
+                    EmpInfo = JsonConvert.DeserializeObject<List<Animals>>(EmpResponse);
+                }
+                //returning the employee list to view
+                return View(EmpInfo);
+            }
         }
 
         public IActionResult OpeningTimes()
---
+++ MvcContozoo/Models/Animals.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcContozoo.Models
+{
+    public class Animals
+    {
+        public int AnimalId { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string Location { get; set; }
+        public string ActiveHours { get; set; }
+        public string Notes { get; set; }
+    }
+}
